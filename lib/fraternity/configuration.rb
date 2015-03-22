dbb6ab0917be223c41aa7734c38f709eca76910b
Jamie Wright <jamie@brilliantfantastic.com>
2015-03-22 00:16:48 -0400

    Add receive_pledge default proc to configuration

--- lib/fraternity/configuration.rb
+++ lib/fraternity/configuration.rb
@@ -1,8 +1,9 @@
 module Fraternity
   class Configuration
-    attr_accessor :database_url, :send_invite
+    attr_accessor :database_url, :send_invite, :receive_pledge
 
     def initialize
+      @receive_pledge = lambda { |pledge| pledge }
       @send_invite = lambda { |pledge| pledge }
     end
   end
